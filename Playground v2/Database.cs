085f9ae98d3db55329ab8c620e9fed44c3d93aa1
Robert048 <Robertkarate@hotmail.com>
2015-03-04 11:56:39 +0100

    j

---
+++ Playground v2/Database.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground_v2
+{
+    class Database
+    {
+        string connectionString = null;
+        OdbcConnection conn;
+
+        public Database()
+        {
+        //get connectionString from the app.config and open the connection.
+        connectionString = ConfigurationManager.ConnectionStrings["Aspen tech"].ConnectionString;
+        conn = new OdbcConnection(connectionString);
+        }
+
+        /// <summary>
+        /// Open a database connection.
+        /// </summary>
+        public bool databaseConnection()
+        {
+            // Start connection
+            try
+            {
+                conn.Open();
+                return true;
+            }
+
+            // If connection failed then display the error
+            catch (Exception e)
+            {
+                Console.WriteLine("Database Connection failed | " + e.Message);
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Close the database connection.
+        /// </summary>
+        public void closeConnection()
+        {
+            conn.Close();
+        }
+
+    }
+}
