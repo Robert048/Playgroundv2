75ee28bc1219ccd6200de2d32dceeceab71c4de6
Robert048 <Robertkarate@hotmail.com>
2015-04-15 09:59:05 +0200

    d

---
+++ Playground v2/dbObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground_v2
+{
+    public class dbObject
+    {
+        public string naam { get; set; }
+
+        public dbObject(string naam)
+        {
+            this.naam = naam;
+        }
+    }
+}
